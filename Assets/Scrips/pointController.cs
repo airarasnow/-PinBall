dedf86670f4debbe4e2d9c66763025ade90b924b
アイララスノウ <airara000snow@gmail.com>
2019-06-28 20:46:57 +0900

    Lesson5のPinBallゲーム課題
    Pinballのゲームファイルです。

---
+++ Assets/Scrips/pointController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pointController : MonoBehaviour
+{
+    private GameObject PointText;
+
+	private int point;
+	// Use this for initialization
+	void Start()
+    {
+		
+		//シーン中のGameOverTextオブジェクトを取得
+		this.PointText = GameObject.Find("pointText");
+    }
+
+    void Update()
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+        if (other.gameObject.tag == "SmallCloudTag")
+        {
+            point += 10;
+        }
+        else if(other.gameObject.tag == "LargeCloudTag")
+        {
+            point += 20;
+        }
+        this.PointText.GetComponent<Text>().text = point.ToString();
+    }
+}
---
+++ Assets/Scrips/pointController2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pointController2 : MonoBehaviour
+{
+    private GameObject PointText;
+
+    private int point;
+    // Use this for initialization
+    void Start()
+    {
+
+        //シーン中のGameOverTextオブジェクトを取得
+        this.PointText = GameObject.Find("pointText");
+    }
+
+    void Update()
+    {
+        this.PointText.GetComponent<Text>().text = point.ToString();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (tag == "LargeCloudTag")
+        {
+            point += 20;
+        }
+    }
+}
